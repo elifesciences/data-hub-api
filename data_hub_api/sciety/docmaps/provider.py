a86ee18eadecf31d73a05720c34c2f7ffba3916c
Daniel Ecer <de-code@users.noreply.github.com>
2022-12-16 07:10:39 +0000

    Implement API using BigQuery query (#6)
    * added bigquery utils
    
    * added initial sql query
    
    * added missing __init__.py
    
    * run bigquery query to provide data (currently raw)
    
    * define initial mapping for query fields
    
    * minor: fix indentation
    
    * added created and updated fields
    
    * added provider field
    
    * added docmap id field
    
    * renamed to get_docmap_item_for_query_result_item
    
    * mount developer credentials

--- data_hub_api/sciety/docmaps/provider.py
+++ data_hub_api/sciety/docmaps/provider.py
@@ -1,13 +1,66 @@
+import logging
 import json
 from pathlib import Path
 from typing import Iterable
 
 
+from data_hub_api.utils.bigquery import (
+    iter_dict_from_bq_query
+)
+from data_hub_api.sciety.docmaps.sql import get_sql_path
+
+
+LOGGER = logging.getLogger(__name__)
+
+DOCMAPS_JSONLD_SCHEMA_URL = 'https://w3id.org/docmaps/context.jsonld'
+
+DOCMAP_ID_PREFIX = 'https://sciety.org/docmaps/v1/articles/'
+DOCMAP_ID_SUFFIX = '/docmap.json'
+
+
+def get_docmap_item_for_query_result_item(query_result_item: dict) -> dict:
+    qc_complete_timestamp_str = query_result_item['qc_complete_timestamp'].isoformat()
+    provider_json = query_result_item['provider_json']
+    LOGGER.debug('provider_json: %r', provider_json)
+    return {
+        '@context': DOCMAPS_JSONLD_SCHEMA_URL,
+        'type': 'docmap',
+        'id': DOCMAP_ID_PREFIX + query_result_item['docmap_id'] + DOCMAP_ID_SUFFIX,
+        'created': qc_complete_timestamp_str,
+        'updated': qc_complete_timestamp_str,
+        'provider': json.loads(provider_json),
+        'first-step': '_:b0',
+        'steps': {
+            '_:b0': {
+                'assertions': [],
+                'inputs': [{
+                    'doi': query_result_item['preprint_doi'],
+                    'url': query_result_item['preprint_url'],
+                }],
+                'actions': []
+            }
+        }
+
+    }
+
+
 class ScietyDocmapsProvider:
+    def __init__(
+        self,
+        gcp_project_name: str = 'elife-data-pipeline'
+    ) -> None:
+        self.gcp_project_name = gcp_project_name
+        self.docmaps_index_query = (
+            Path(get_sql_path('docmaps_index.sql')).read_text(encoding='utf-8')
+        )
+
     def iter_docmaps(self) -> Iterable[dict]:
-        docmaps_path = Path('./data/docmaps/')
-        for docmaps_file_path in docmaps_path.iterdir():
-            yield json.loads(docmaps_file_path.read_bytes())
+        bq_result_iterable = iter_dict_from_bq_query(
+            self.gcp_project_name,
+            self.docmaps_index_query
+        )
+        for bq_result in bq_result_iterable:
+            yield get_docmap_item_for_query_result_item(bq_result)
 
     def get_docmaps_index(self) -> dict:
         article_docmaps_list = list(self.iter_docmaps())
--- tests/unit_tests/sciety/docmaps/provider_test.py
+++ tests/unit_tests/sciety/docmaps/provider_test.py
@@ -1,7 +1,95 @@
-from data_hub_api.sciety.docmaps.provider import ScietyDocmapsProvider
+import json
+from datetime import datetime
+from unittest.mock import patch, MagicMock
+from typing import Iterable
+
+import pytest
+
+from data_hub_api.sciety.docmaps import provider as provider_module
+from data_hub_api.sciety.docmaps.provider import (
+    get_docmap_item_for_query_result_item,
+    ScietyDocmapsProvider,
+    DOCMAPS_JSONLD_SCHEMA_URL,
+    DOCMAP_ID_PREFIX,
+    DOCMAP_ID_SUFFIX
+)
+
+
+DOI_1 = '10.1101.test/doi1'
+DOI_2 = '10.1101.test/doi2'
+
+
+DOCMAPS_QUERY_RESULT_ITEM_1 = {
+    'manuscript_id': 'manuscript_id_1',
+    'qc_complete_timestamp': datetime.fromisoformat('2022-01-01T01:02:03+00:00'),
+    'preprint_doi': DOI_1,
+    'preprint_version': None,
+    'preprint_url': f'https://doi.org/{DOI_1}',
+    'docmap_id': 'docmap_id_1',
+    'provider_json': '{"id": "provider_1"}'
+}
+
+
+@pytest.fixture(name='iter_dict_from_bq_query_mock', autouse=True)
+def _iter_dict_from_bq_query_mock() -> Iterable[MagicMock]:
+    with patch.object(provider_module, 'iter_dict_from_bq_query') as mock:
+        yield mock
+
+
+class TestGetDocmapsItemForQueryResultItem:
+    def test_should_populate_context(self):
+        docmaps_item = get_docmap_item_for_query_result_item(DOCMAPS_QUERY_RESULT_ITEM_1)
+        assert docmaps_item['@context'] == DOCMAPS_JSONLD_SCHEMA_URL
+
+    def test_should_populate_type(self):
+        docmaps_item = get_docmap_item_for_query_result_item(DOCMAPS_QUERY_RESULT_ITEM_1)
+        assert docmaps_item['type'] == 'docmap'
+
+    def test_should_add_prefix_and_suffix_to_id(self):
+        docmaps_item = get_docmap_item_for_query_result_item(DOCMAPS_QUERY_RESULT_ITEM_1)
+        assert docmaps_item['id'] == (
+            DOCMAP_ID_PREFIX + DOCMAPS_QUERY_RESULT_ITEM_1['docmap_id'] + DOCMAP_ID_SUFFIX
+        )
+
+    def test_should_populate_create_and_updated_timestamp_with_qc_complete_timestamp(self):
+        docmaps_item = get_docmap_item_for_query_result_item(DOCMAPS_QUERY_RESULT_ITEM_1)
+        assert docmaps_item['created'] == (
+            DOCMAPS_QUERY_RESULT_ITEM_1['qc_complete_timestamp'].isoformat()
+        )
+        assert docmaps_item['updated'] == docmaps_item['created']
+
+    def test_should_parse_and_include_provider_json(self):
+        docmaps_item = get_docmap_item_for_query_result_item(DOCMAPS_QUERY_RESULT_ITEM_1)
+        assert docmaps_item['provider'] == json.loads(
+            DOCMAPS_QUERY_RESULT_ITEM_1['provider_json']
+        )
+
+    def test_should_populate_first_step_input_doi_and_url(self):
+        docmaps_item = get_docmap_item_for_query_result_item(DOCMAPS_QUERY_RESULT_ITEM_1)
+        first_step_key = docmaps_item['first-step']
+        first_step = docmaps_item['steps'][first_step_key]
+        first_step_input = first_step['inputs']
+        assert len(first_step_input) == 1
+        assert first_step_input[0]['doi'] == DOCMAPS_QUERY_RESULT_ITEM_1['preprint_doi']
+        assert first_step_input[0]['url'] == DOCMAPS_QUERY_RESULT_ITEM_1['preprint_url']
+
+    def test_should_populate_empty_first_step_assertions_and_actions(self):
+        docmaps_item = get_docmap_item_for_query_result_item(DOCMAPS_QUERY_RESULT_ITEM_1)
+        first_step_key = docmaps_item['first-step']
+        first_step = docmaps_item['steps'][first_step_key]
+        assert not first_step['assertions']
+        assert not first_step['actions']
 
 
 class TestScietyDocmapsProvider:
-    def test_should_create_index_with_non_empty_articles(self):
+    def test_should_create_index_with_non_empty_articles(
+        self,
+        iter_dict_from_bq_query_mock: MagicMock
+    ):
+        iter_dict_from_bq_query_mock.return_value = iter([
+            DOCMAPS_QUERY_RESULT_ITEM_1
+        ])
         docmaps_index = ScietyDocmapsProvider().get_docmaps_index()
-        assert docmaps_index['articles']
+        assert docmaps_index['articles'] == [
+            get_docmap_item_for_query_result_item(DOCMAPS_QUERY_RESULT_ITEM_1)
+        ]
