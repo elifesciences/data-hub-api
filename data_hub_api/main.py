8ce9fbf2282cb756794124c048c1dc5d816f7ba9
Daniel Ecer <de-code@users.noreply.github.com>
2022-12-20 12:56:02 +0000

    added /public-reviews endpoint

--- data_hub_api/main.py
+++ data_hub_api/main.py
@@ -10,7 +10,16 @@ LOGGER = logging.getLogger(__name__)
 
 def create_app():
     app = FastAPI()
-    enhanced_preprints_docmaps_provider = EnhancedPreprintsDocmapsProvider()
+
+    enhanced_preprints_docmaps_provider = EnhancedPreprintsDocmapsProvider(
+        only_include_reviewed_preprint_type=True,
+        only_include_evaluated_preprints=False
+    )
+
+    public_reviews_docmaps_provider = EnhancedPreprintsDocmapsProvider(
+        only_include_reviewed_preprint_type=False,
+        only_include_evaluated_preprints=True
+    )
 
     @app.get("/")
     def get_root():
@@ -19,5 +28,9 @@ def create_app():
     @app.get("/enhanced-preprints/docmaps/v1/index")
     def get_enhanced_preprints_docmaps_index():
         return enhanced_preprints_docmaps_provider.get_docmaps_index()
+
+    @app.get("/public-reviews/docmaps/v1/index")
+    def get_public_reviews_docmaps_index():
+        return public_reviews_docmaps_provider.get_docmaps_index()
 
     return app
--- tests/unit_tests/main_test.py
+++ tests/unit_tests/main_test.py
@@ -1,5 +1,5 @@
-from unittest.mock import patch, MagicMock
-from typing import Iterable
+from unittest.mock import call, patch, MagicMock
+from typing import Iterable, Sequence
 
 import pytest
 
@@ -15,11 +15,29 @@ def _enhanced_preprints_docmaps_provider_class_mock() -> Iterable[MagicMock]:
         yield mock
 
 
+@pytest.fixture(name='docmaps_provider_mock_list')
+def _docmaps_provider_mock_list(
+    enhanced_preprints_docmaps_provider_class_mock: MagicMock
+) -> Sequence[MagicMock]:
+    mock_list = [MagicMock(name='enhanced-reviews'), MagicMock(name='public-reviews')]
+    enhanced_preprints_docmaps_provider_class_mock.side_effect = mock_list
+    return mock_list
+
+
 @pytest.fixture(name='enhanced_preprints_docmaps_provider_mock')
 def _enhanced_preprints_docmaps_provider_mock(
-    enhanced_preprints_docmaps_provider_class_mock: MagicMock
+    docmaps_provider_mock_list: Sequence[MagicMock]
 ) -> MagicMock:
-    return enhanced_preprints_docmaps_provider_class_mock.return_value
+    return docmaps_provider_mock_list[0]
+
+
+@pytest.fixture(name='public_reviews_docmaps_provider_mock')
+def _public_reviews_docmaps_provider_mock(
+    docmaps_provider_mock_list: Sequence[MagicMock]
+) -> MagicMock:
+    return docmaps_provider_mock_list[1]
+
+
 
 
 def test_read_main():
@@ -30,7 +48,7 @@ def test_read_main():
 
 
 class TestGetEnhancedPreprintsDocmapsIndex:
-    def test_should_return_json_with_docmaps_list(
+    def test_should_return_json_with_docmaps_list_from_enhanced_preprint_provider(
         self,
         enhanced_preprints_docmaps_provider_mock: MagicMock
     ):
@@ -39,3 +57,32 @@ class TestGetEnhancedPreprintsDocmapsIndex:
         client = TestClient(create_app())
         response = client.get("/enhanced-preprints/docmaps/v1/index")
         assert response.json() == docmaps_index
+
+    def test_should_return_json_with_docmaps_list_from_public_review_provider(
+        self,
+        public_reviews_docmaps_provider_mock: MagicMock
+    ):
+        docmaps_index = [{'docmaps': [{'id': 'docmap_1'}, {'id': 'docmap_2'}]}]
+        public_reviews_docmaps_provider_mock.get_docmaps_index.return_value = docmaps_index
+        client = TestClient(create_app())
+        response = client.get("/public-reviews/docmaps/v1/index")
+        assert response.json() == docmaps_index
+
+    def test_should_pass_correct_parameters_to_provider_class(
+        self,
+        enhanced_preprints_docmaps_provider_class_mock: MagicMock
+    ):
+        create_app()
+        enhanced_preprints_docmaps_provider_class_mock.assert_has_calls(
+            [
+                call(
+                    only_include_reviewed_preprint_type=True,
+                    only_include_evaluated_preprints=False
+                ),
+                call(
+                    only_include_reviewed_preprint_type=False,
+                    only_include_evaluated_preprints=True
+                )
+            ],
+            any_order=False
+        )
