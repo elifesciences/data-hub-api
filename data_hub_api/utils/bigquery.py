176f46a5bf9695272ddc80289b25d4970c6f3f1b
Hazal <HazalCiplak@users.noreply.github.com>
2023-01-06 14:42:48 +0000

    Bug fixing: get-by-doi endpoint (#34)
    * removed unnecessary docmap_id field
    
    * added doi in quatos in docmap id
    
    * used query parameter for preprint doi to avoid sql injection

--- data_hub_api/utils/bigquery.py
+++ data_hub_api/utils/bigquery.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Iterable
+from typing import Any, Iterable, Optional, Sequence
 
 from google.cloud import bigquery
 from google.cloud.bigquery.table import RowIterator
@@ -13,10 +13,12 @@ def get_bq_client(project_name: str) -> bigquery.Client:
 
 def get_bq_result_from_bq_query(
     project_name: str,
-    query: str
+    query: str,
+    query_parameters: Optional[Sequence[Any]] = tuple()
 ) -> RowIterator:
     client = get_bq_client(project_name=project_name)
-    query_job = client.query(query)  # Make an API request.
+    job_config = bigquery.QueryJobConfig(query_parameters=query_parameters)
+    query_job = client.query(query, job_config=job_config)  # Make an API request.
     bq_result = query_job.result()  # Waits for query to finish
     LOGGER.debug('bq_result: %r', bq_result)
     return bq_result
@@ -24,9 +26,14 @@ def get_bq_result_from_bq_query(
 
 def iter_dict_from_bq_query(
     project_name: str,
-    query: str
+    query: str,
+    query_parameters: Optional[Sequence[Any]] = tuple()
 ) -> Iterable[dict]:
-    bq_result = get_bq_result_from_bq_query(project_name=project_name, query=query)
+    bq_result = get_bq_result_from_bq_query(
+        project_name=project_name,
+        query=query,
+        query_parameters=query_parameters
+    )
     for row in bq_result:
         LOGGER.debug('row: %r', row)
         yield dict(row.items())
