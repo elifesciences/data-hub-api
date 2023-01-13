95a4d87cabfa7fddd9a0143413af89a1e2e1c4c6
Daniel Ecer <de-code@users.noreply.github.com>
2023-01-13 12:48:31 +0000

    implemented max age

--- data_hub_api/utils/cache.py
+++ data_hub_api/utils/cache.py
@@ -1,3 +1,4 @@
+from time import monotonic
 from typing import Callable, Optional,  Protocol, TypeVar
 
 
@@ -14,17 +15,32 @@ class SingleObjectCache(Protocol[T]):
 
 class SingleObjectInMemoryCache(SingleObjectCache[T]):
 
-    def __init__(self, initial_value: Optional[T] = None) -> None:
+    def __init__(
+        self,
+        initial_value: Optional[T] = None,
+        max_age_in_seconds: Optional[float] = None
+    ) -> None:
         self._value = initial_value
+        self.max_age_in_seconds = max_age_in_seconds
+        self._last_updated_time: Optional[float] = None
+
+    def _is_max_age_reached(self, now: float) -> bool:
+        return bool(
+            self.max_age_in_seconds
+            and self._last_updated_time is not None
+            and (now - self._last_updated_time > self.max_age_in_seconds)
+        )
 
     def get(self) -> Optional[T]:
         return self._value
 
     def get_or_load(self, load_fn: Callable[[], T]) -> T:
-        result = self.get()
-        if result is not None:
+        now = monotonic()
+        result = self._value
+        if result is not None and not self._is_max_age_reached(now):
             return result
         result = load_fn()
         assert result is not None
         self._value = result
+        self._last_updated_time = now
         return result
--- tests/unit_tests/utils/cache_test.py
+++ tests/unit_tests/utils/cache_test.py
@@ -1,8 +1,18 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
+from typing import Iterable
 
+import pytest
+
+import data_hub_api.utils.cache as cache_module
 from data_hub_api.utils.cache import (
     SingleObjectInMemoryCache
 )
+
+
+@pytest.fixture(name='monotonic_mock')
+def _monotonic_mock() -> Iterable[MagicMock]:
+    with patch.object(cache_module, 'monotonic') as mock:
+        yield mock
 
 
 class TestSingleObjectInMemoryCache:
@@ -23,3 +33,16 @@ class TestSingleObjectInMemoryCache:
         result = cache.get_or_load(load_fn=load_fn)
         assert result == 'value_1'
         assert load_fn.call_count == 1
+
+    def test_should_reload_if_max_age_reached(self, monotonic_mock: MagicMock):
+        cache = SingleObjectInMemoryCache[str](
+            max_age_in_seconds=60
+        )
+        load_fn = MagicMock(name='load_fn')
+        load_fn.side_effect = ['value_1', 'value_2']
+        monotonic_mock.return_value = 100
+        cache.get_or_load(load_fn=load_fn)
+        monotonic_mock.return_value = 200
+        result = cache.get_or_load(load_fn=load_fn)
+        assert result == 'value_2'
+        assert load_fn.call_count == 2
