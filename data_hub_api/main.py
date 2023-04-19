e68f1ac378774ed9af8e1e57e662fc82740ec7fe
HazalCiplak <hazalciplak@gmail.com>
2023-04-19 17:02:35 +0100

    updated ADDITIONAL_PREPRINT_DOIS to ADDITIONAL_MANUSCRIPT_IDS

--- data_hub_api/main.py
+++ data_hub_api/main.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 
 from data_hub_api.utils.cache import InMemorySingleObjectCache
-from data_hub_api.docmaps.provider import ADDITIONAL_PREPRINT_DOIS, DocmapsProvider
+from data_hub_api.docmaps.provider import ADDITIONAL_MANUSCRIPT_IDS, DocmapsProvider
 
 
 LOGGER = logging.getLogger(__name__)
@@ -41,7 +41,7 @@ def create_app():
     enhanced_preprints_docmaps_provider = DocmapsProvider(
         only_include_reviewed_preprint_type=True,
         only_include_evaluated_preprints=False,
-        additionally_include_preprint_dois=ADDITIONAL_PREPRINT_DOIS,
+        additionally_include_manuscript_ids=ADDITIONAL_MANUSCRIPT_IDS,
         query_results_cache=InMemorySingleObjectCache(max_age_in_seconds=max_age_in_seconds)
     )
 
