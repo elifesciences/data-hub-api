7e82125664218a35b5e3cc4a3ccb54266f459544
HazalCiplak <hazalciplak@gmail.com>
2023-04-25 11:12:36 +0100

    linting (inconsistent-return-statements)

--- data_hub_api/utils/util.py
+++ data_hub_api/utils/util.py
@@ -9,3 +9,4 @@ def get_previous_element_of_given_element_in_given_list(
         if index != 0 and element == given_element:
             return given_list[index-1]
         continue
+    return None
