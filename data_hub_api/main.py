b6d55fbb4dcdc8030d320b678d3f6a9ac963a85b
HazalCiplak <hazalciplak@gmail.com>
2023-06-13 14:29:59 +0100

    added v2 to the main

--- data_hub_api/main.py
+++ data_hub_api/main.py
@@ -4,9 +4,11 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from data_hub_api.config import ADDITIONAL_MANUSCRIPT_IDS
 from data_hub_api.docmaps.v1.api_router import create_docmaps_router as create_docmaps_router_v1
+from data_hub_api.docmaps.v2.api_router import create_docmaps_router
 
 from data_hub_api.utils.cache import InMemorySingleObjectCache
 from data_hub_api.docmaps.v1.provider import DocmapsProviderV1
+from data_hub_api.docmaps.v2.provider import DocmapsProvider
 
 
 LOGGER = logging.getLogger(__name__)
@@ -17,7 +19,14 @@ def create_app():
 
     max_age_in_seconds = 60 * 60  # 1 hour
 
-    enhanced_preprints_docmaps_provider = DocmapsProviderV1(
+    enhanced_preprints_docmaps_provider_v1 = DocmapsProviderV1(
+        only_include_reviewed_preprint_type=True,
+        only_include_evaluated_preprints=False,
+        additionally_include_manuscript_ids=ADDITIONAL_MANUSCRIPT_IDS,
+        query_results_cache=InMemorySingleObjectCache(max_age_in_seconds=max_age_in_seconds)
+    )
+
+    enhanced_preprints_docmaps_provider = DocmapsProvider(
         only_include_reviewed_preprint_type=True,
         only_include_evaluated_preprints=False,
         additionally_include_manuscript_ids=ADDITIONAL_MANUSCRIPT_IDS,
@@ -38,6 +47,13 @@ def create_app():
 
     app.include_router(
         create_docmaps_router_v1(
+            enhanced_preprints_docmaps_provider_v1
+        ),
+        prefix='/enhanced-preprints/docmaps'
+    )
+
+    app.include_router(
+        create_docmaps_router(
             enhanced_preprints_docmaps_provider
         ),
         prefix='/enhanced-preprints/docmaps'
