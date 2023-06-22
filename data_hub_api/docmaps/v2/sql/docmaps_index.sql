1335f40e52f39ff4584e28ce01ca360e1a0262b3
Hazal <HazalCiplak@users.noreply.github.com>
2023-06-22 13:36:01 +0100

    added VOR filter to docmaps index query (#86)

--- data_hub_api/docmaps/v2/sql/docmaps_index.sql
+++ data_hub_api/docmaps/v2/sql/docmaps_index.sql
@@ -105,6 +105,7 @@ t_reviewed_preprints_with_under_review_process AS (
   FROM `elife-data-pipeline.prod.v_manuscript_with_matching_preprint_server_doi`
   WHERE preprint_doi IS NOT NULL
   AND is_or_was_under_review -- to include the "six" additional manuscripts
+  AND long_manuscript_identifier NOT LIKE '%VOR%'
 ),
 
 t_result_with_evaluations AS (
