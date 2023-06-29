53af7eab7c9e40f60ff4f3950e0965d014939453
HazalCiplak <hazalciplak@gmail.com>
2023-06-29 15:00:51 +0100

    made elife_doi required

--- data_hub_api/docmaps/v2/codecs/evaluation.py
+++ data_hub_api/docmaps/v2/codecs/evaluation.py
@@ -32,17 +32,13 @@ DOCMAP_EVALUATION_TYPE_FOR_REVIEW_ARTICLE = 'review-article'
 
 def get_elife_evaluation_doi(
     elife_doi_version_str: str,
-    elife_doi: Optional[str] = None,
-    evaluation_suffix: Optional[str] = None
-) -> Optional[str]:
+    elife_doi: str,
+    evaluation_suffix: str
+) -> str:
     elife_version_doi = get_elife_manuscript_version_doi(
         elife_doi=elife_doi,
         elife_doi_version_str=elife_doi_version_str
     )
-    if not elife_version_doi:
-        return None
-    if not evaluation_suffix:
-        return elife_version_doi
     return elife_version_doi + '.' + evaluation_suffix
 
 
