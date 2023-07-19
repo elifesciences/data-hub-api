6b32f156f798688443ee5ccaa116a27606479e6e
Hazal <HazalCiplak@users.noreply.github.com>
2023-07-19 13:57:55 +0100

    Improve typing tests (#99)
    * added ApiManuscriptVersionInput in to test data
    
    * added 'ApiInput' to test_data.py
    
    * added 'ApiEditorDetailInput' to test_data.py
    
    * removed "False" from 'ApiEvaluationInput' TypedDict
    
    * removed "False" from ApiEditorDetailInput TypedDict
    
    * removed "total=False" from ApiInput TypedDict
    
    * removed "total=False" from ApiManuscriptVersionInput TypedDict
    
    * removed unused MECA_PATH_3 = 'meca_path_3' test data
    
    * removed unnessary comments

--- data_hub_api/docmaps/v2/api_input_typing.py
+++ data_hub_api/docmaps/v2/api_input_typing.py
@@ -11,8 +11,7 @@ ApiEvaluationInput = TypedDict(
         'uri': str,
         'source_version': str,
         'evaluation_suffix': str
-    },
-    total=False
+    }
 )
 
 ApiEditorDetailInput = TypedDict(
@@ -21,8 +20,7 @@ ApiEditorDetailInput = TypedDict(
         'name': str,
         'institution': str,
         'country': Optional[str],
-    },
-    total=False
+    }
 )
 
 ApiManuscriptVersionInput = TypedDict(
@@ -31,18 +29,17 @@ ApiManuscriptVersionInput = TypedDict(
         'long_manuscript_identifier': str,
         'position_in_overall_stage': int,
         'qc_complete_timestamp': datetime,
-        'under_review_timestamp': datetime,
+        'under_review_timestamp': Optional[datetime],
         'editor_details': Sequence[ApiEditorDetailInput],
         'senior_editor_details': Sequence[ApiEditorDetailInput],
         'preprint_url': str,
         'elife_doi_version_str': str,
         'preprint_doi': str,
-        'preprint_version': str,
-        'preprint_published_at_date': date,
-        'meca_path': str,
+        'preprint_version': Optional[str],
+        'preprint_published_at_date': Optional[date],
+        'meca_path': Optional[str],
         'evaluations': Sequence[ApiEvaluationInput]
-    },
-    total=False
+    }
 )
 
 
@@ -55,6 +52,5 @@ ApiInput = TypedDict(
         'license': str,
         'is_reviewed_preprint_type': bool,
         'manuscript_versions': Sequence[ApiManuscriptVersionInput]
-    },
-    total=False
+    }
 )
--- tests/unit_tests/docmaps/v2/test_data.py
+++ tests/unit_tests/docmaps/v2/test_data.py
@@ -1,5 +1,12 @@
 from datetime import date, datetime
 
+from data_hub_api.docmaps.v2.api_input_typing import (
+    ApiEditorDetailInput,
+    ApiEvaluationInput,
+    ApiInput,
+    ApiManuscriptVersionInput
+)
+
 MANUSCRIPT_ID_1 = 'manuscript_id_1'
 
 LONG_MANUSCRIPT_ID_1 = 'long_manuscript_id'
@@ -28,12 +35,10 @@ ELIFE_DOI_VERSION_STR_3 = 'elife_doi_version_str_3'
 
 MECA_PATH_1 = 'meca_path_1'
 MECA_PATH_2 = 'meca_path_2'
-MECA_PATH_3 = 'meca_path_3'
-
 
 LICENSE_1 = 'license_1'
 
-MANUSCRIPT_VERSION_1: dict = {
+MANUSCRIPT_VERSION_1: ApiManuscriptVersionInput = {
     'long_manuscript_identifier': LONG_MANUSCRIPT_ID_1,
     'position_in_overall_stage': 1,
     'qc_complete_timestamp': datetime.fromisoformat('2022-01-01T01:02:03+00:00'),
@@ -49,7 +54,7 @@ MANUSCRIPT_VERSION_1: dict = {
     'evaluations': []
 }
 
-MANUSCRIPT_VERSION_2: dict = {
+MANUSCRIPT_VERSION_2: ApiManuscriptVersionInput = {
     'long_manuscript_identifier': LONG_MANUSCRIPT_ID_2,
     'position_in_overall_stage': 2,
     'qc_complete_timestamp': datetime.fromisoformat('2022-02-02T02:02:02+00:00'),
@@ -65,24 +70,24 @@ MANUSCRIPT_VERSION_2: dict = {
     'evaluations': []
 }
 
-MANUSCRIPT_VOR_VERSION_1: dict = {
+MANUSCRIPT_VOR_VERSION_1: ApiManuscriptVersionInput = {
     'long_manuscript_identifier': LONG_MANUSCRIPT_VOR_ID_1,
     'position_in_overall_stage': 2,
     'qc_complete_timestamp': datetime.fromisoformat('2022-04-02T02:02:02+00:00'),
     'under_review_timestamp': None,
     'editor_details': [],
     'senior_editor_details': [],
-    'preprint_url': None,
+    'preprint_url': PREPRINT_LINK_3,
     'elife_doi_version_str': ELIFE_DOI_VERSION_STR_3,
-    'preprint_doi': None,
+    'preprint_doi': DOI_2,
     'preprint_version': None,
     'preprint_published_at_date': None,
     'meca_path': None,
     'evaluations': []
 }
 
-DOCMAPS_QUERY_RESULT_ITEM_1: dict = {
-    'publisher_json': '{"id": "publisher_1"}',
+DOCMAPS_QUERY_RESULT_ITEM_1: ApiInput = {
+    'publisher_json': {"id": "publisher_1"},
     'manuscript_id': 'manuscript_id_1',
     'elife_doi': ELIFE_DOI_1,
     'license': LICENSE_1,
@@ -90,8 +95,8 @@ DOCMAPS_QUERY_RESULT_ITEM_1: dict = {
     'manuscript_versions': [MANUSCRIPT_VERSION_1]
 }
 
-DOCMAPS_QUERY_RESULT_ITEM_2: dict = {
-    'publisher_json': '{"id": "publisher_1"}',
+DOCMAPS_QUERY_RESULT_ITEM_2: ApiInput = {
+    'publisher_json': {"id": "publisher_1"},
     'manuscript_id': 'manuscript_id_1',
     'elife_doi': ELIFE_DOI_1,
     'license': LICENSE_1,
@@ -111,7 +116,7 @@ ANNOTATION_CREATED_TIMESTAMP_1 = datetime.fromisoformat('2020-01-01T01:02:03+00:
 ANNOTATION_CREATED_TIMESTAMP_2 = datetime.fromisoformat('2021-01-01T01:02:03+00:00')
 ANNOTATION_CREATED_TIMESTAMP_3 = datetime.fromisoformat('2022-01-01T01:02:03+00:00')
 
-DOCMAPS_QUERY_RESULT_EVALUATION_1 = {
+DOCMAPS_QUERY_RESULT_EVALUATION_1: ApiEvaluationInput = {
     'hypothesis_id': HYPOTHESIS_ID_1,
     'annotation_created_timestamp': ANNOTATION_CREATED_TIMESTAMP_1,
     'tags': [],
@@ -120,7 +125,7 @@ DOCMAPS_QUERY_RESULT_EVALUATION_1 = {
     'evaluation_suffix': EVALUATION_SUFFIX_1
 }
 
-DOCMAPS_QUERY_RESULT_EVALUATION_2 = {
+DOCMAPS_QUERY_RESULT_EVALUATION_2: ApiEvaluationInput = {
     'hypothesis_id': HYPOTHESIS_ID_2,
     'annotation_created_timestamp': ANNOTATION_CREATED_TIMESTAMP_2,
     'tags': [],
@@ -129,33 +134,33 @@ DOCMAPS_QUERY_RESULT_EVALUATION_2 = {
     'evaluation_suffix': EVALUATION_SUFFIX_2
 }
 
-MANUSCRIPT_VERSION_WITH_EVALUATIONS_1 = {
-    **MANUSCRIPT_VERSION_1,
+MANUSCRIPT_VERSION_WITH_EVALUATIONS_1: ApiManuscriptVersionInput = {
+    **MANUSCRIPT_VERSION_1,  # type: ignore
     'evaluations': [DOCMAPS_QUERY_RESULT_EVALUATION_1]
 }
 
-MANUSCRIPT_VERSION_WITH_EVALUATIONS_2 = {
-    **MANUSCRIPT_VERSION_2,
+MANUSCRIPT_VERSION_WITH_EVALUATIONS_2: ApiManuscriptVersionInput = {
+    **MANUSCRIPT_VERSION_2,  # type: ignore
     'evaluations': [DOCMAPS_QUERY_RESULT_EVALUATION_2]
 }
 
 DOCMAPS_QUERY_RESULT_ITEM_WITH_EVALUATIONS = {
-    **DOCMAPS_QUERY_RESULT_ITEM_1,
+    **DOCMAPS_QUERY_RESULT_ITEM_1,  # type: ignore
     'manuscript_versions': [MANUSCRIPT_VERSION_WITH_EVALUATIONS_1]
 }
 
 DOCMAPS_QUERY_RESULT_ITEM_WITH_VOR_VERSION = {
-    **DOCMAPS_QUERY_RESULT_ITEM_1,
+    **DOCMAPS_QUERY_RESULT_ITEM_1,  # type: ignore
     'manuscript_versions': [MANUSCRIPT_VOR_VERSION_1]
 }
 
-EDITOR_DETAIL_1 = {
+EDITOR_DETAIL_1: ApiEditorDetailInput = {
     'name': 'editor_name_1',
     'institution': 'editor_institution_1',
     'country': 'editor_country_1'
 }
 
-SENIOR_EDITOR_DETAIL_1 = {
+SENIOR_EDITOR_DETAIL_1: ApiEditorDetailInput = {
     'name': 'senior_editor_name_1',
     'institution': 'senior_editor_institution_1',
     'country': 'senior_editor_country_1'
