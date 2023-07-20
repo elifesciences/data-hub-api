304b008ece10887e101745cfdb992cfd38ad69ef
Hazal <HazalCiplak@users.noreply.github.com>
2023-07-20 10:48:01 +0100

    fixed the bug with dateutil.parser._parser.ParserError (#100)

--- tests/unit_tests/utils/format_datetime_test.py
+++ tests/unit_tests/utils/format_datetime_test.py
@@ -19,3 +19,9 @@ class TestFormatDatetimeWithUtcOffset:
             date_string='2023-05-05T01:02:03+00:00'
         )
         assert result == '2023-05-05T01:02:03+00:00'
+
+    def test_should_return_none_when_the_datetime_string_is_none(self):
+        result = format_datetime_with_utc_offset(
+            date_string=None
+        )
+        assert not result
