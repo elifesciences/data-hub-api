5803db911cc5fd0f530657d310370cbdd33d4170
HazalCiplak <hazalciplak@gmail.com>
2024-01-15 13:23:51 +0000

    added test for 88984

--- tests/regression_tests/docmaps/example_manuscript_test.py
+++ tests/regression_tests/docmaps/example_manuscript_test.py
@@ -14,7 +14,8 @@ MANUSCRIPT_ID_LIST = [
     '89891',  # medRxiv preprint server
     '86873',  # arXiv preprint server
     '87193',  # OSF preprint server
-    '87198'  # Research Square preprint server
+    '87198',  # Research Square preprint server
+    '88984'  # hypothesis annotation links to v10 but we decided to use v11 for prodcution process
 ]
 
 
