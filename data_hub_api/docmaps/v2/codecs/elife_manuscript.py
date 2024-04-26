b64441665ed8a9101d641541e98c2dec1083d6a8
Nathan Lisgo <nathan@lisgo.co.uk>
2024-04-26 15:10:10 +0100

    Normalise complement types in docmap generation

--- data_hub_api/docmaps/v2/codecs/elife_manuscript.py
+++ data_hub_api/docmaps/v2/codecs/elife_manuscript.py
@@ -1,3 +1,4 @@
+import re
 from typing import Iterable, Optional, Sequence
 from data_hub_api.config import (
     DOI_ROOT_URL,
@@ -104,8 +105,12 @@ def iter_elife_manuscript_part_of_section_complement_for_one_record(
 ) -> Iterable[DocmapPartOfComplement]:
     if related_content['manuscript_id']:
         assert related_content['manuscript_type']
+        manuscript_type = related_content['manuscript_type'].lower().strip()
+        cleaned_manuscript_type = re.sub(r'[^a-z0-9 ]', ' ', manuscript_type)
+        manuscript_type_parts = re.split(r'\s+', cleaned_manuscript_type)
+        capitalized_parts = [part.capitalize() for part in manuscript_type_parts[1:]]
         yield {
-            'type': related_content['manuscript_type'],
+            'type': manuscript_type_parts[0] + ''.join(capitalized_parts),
             'url': 'https://elifesciences.org/articles/' + related_content['manuscript_id'],
             'title': related_content['manuscript_title'],
             'description': related_content['manuscript_authors_csv']
@@ -113,7 +118,7 @@ def iter_elife_manuscript_part_of_section_complement_for_one_record(
     if related_content['collection_id']:
         assert related_content['collection_curator_name']
         yield {
-            'type': 'Collection',
+            'type': 'collection',
             'url': ('https://elifesciences.org/collections/'
                     + related_content['collection_id']
                     + '/meta-research-a-collection-of-articles'),
@@ -127,7 +132,7 @@ def iter_elife_manuscript_part_of_section_complement_for_one_record(
         }
     if related_content['podcast_id']:
         yield {
-            'type': 'Podcast',
+            'type': 'podcastChapterEpisode',
             'url': (
                 'https://elifesciences.org/podcast/episode'
                 + str(related_content['podcast_id'])
