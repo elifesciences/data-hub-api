a916e704d3df44de86ebaf30308a86662a1ad6cb
Hazal <HazalCiplak@users.noreply.github.com>
2024-07-08 15:16:59 +0100

    Kotahi DocMaps: Format evaluations as HTML (#201)
    * introduced convert_plain_text_to_html
    
    * implemented get_evaluation_html_by_evaluation_id
    
    * splited when there is space in the blank lines
    
    * escaped special characters
    
    * made first paragraph bold
    
    * Added br when there is a single line new line
    
    * test_should_remove_trailing_whitespace
    
    * preserved leading whitespace in pragraphs
    
    * ignored additional new lines between paragraphs
    
    * updated response class  as HTMLResponse
    
    * updated mocks in router test and added check for content type

---
+++ data_hub_api/utils/html.py
@@ -0,0 +1,22 @@
+import re
+import html
+
+
+def convert_paragraph_text_to_html(paragraph_text: str, is_bold: bool = False) -> str:
+    lines = [line.rstrip() for line in paragraph_text.split('\n')]
+    inner_html = html.escape('\n'.join(lines)).replace('\n', '<br/>\n')
+    if is_bold:
+        inner_html = f'<strong>{inner_html}</strong>'
+    return f'<p>{inner_html}</p>'
+
+
+def convert_plain_text_to_html(plain_text: str, is_first_paragraph_bold: bool = False) -> str:
+    if not plain_text:
+        return ''
+    paragraphs = re.split(r'\n\s*\n', plain_text)
+
+    html_paragraphs = [
+        convert_paragraph_text_to_html(paragraph, is_bold=(is_first_paragraph_bold and index == 0))
+        for index, paragraph in enumerate(paragraphs)
+    ]
+    return '\n\n'.join(html_paragraphs)
