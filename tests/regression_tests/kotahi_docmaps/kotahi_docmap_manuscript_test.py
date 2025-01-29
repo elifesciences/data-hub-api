ded5b487ee14a7c048e21876fc4c8dd49ef05d5e
Hazal <HazalCiplak@users.noreply.github.com>
2025-01-29 16:51:34 +0000

    added evaluations to regression test (#273)

--- tests/regression_tests/kotahi_docmaps/kotahi_docmap_manuscript_test.py
+++ tests/regression_tests/kotahi_docmaps/kotahi_docmap_manuscript_test.py
@@ -1,7 +1,7 @@
 import os
+import json
 import pytest
 import requests
-import json
 
 DEFAULT_DATA_HUB_API_REGRESSION_TEST_URL_PREFIX = 'http://localhost:8000'
 DATA_HUB_API_REGRESSION_TEST_URL_PREFIX_ENV = 'DATA_HUB_API_REGRESSION_TEST_URL_PREFIX'
@@ -9,9 +9,17 @@ DATA_HUB_API_REGRESSION_TEST_URL_PREFIX_ENV = 'DATA_HUB_API_REGRESSION_TEST_URL_
 DOCMAP_BY_MANUSCRIPT_PATH = (
     '/kotahi/docmaps/v1/by-publisher/elife/get-by-manuscript-id'
 )
+EVALUATION_BY_ID_PATH = (
+    '/kotahi/docmaps/v1/evaluation/get-by-evaluation-id'
+)
 
 MANUSCRIPT_ID_LIST = [
     '93934'  # evaluations for 2 versions of manuscript
+]
+
+EVALUATION_ID_LIST = [
+    'eLife-RP-TR-2023-93934:evaluation-summary:1',
+    'eLife-RP-TR-2023-93934:review-article:1'
 ]
 
 
@@ -30,12 +38,20 @@ def _regression_test_docmap_by_manuscript_url(
     return regression_test_url_prefix + DOCMAP_BY_MANUSCRIPT_PATH
 
 
+@pytest.fixture(name='regression_test_evaluations_url')
+def _regression_test_evaluations_url(
+    regression_test_url_prefix: str
+) -> str:
+    return regression_test_url_prefix + EVALUATION_BY_ID_PATH
+
+
 def test_docmaps_should_not_be_available_for_manuscript_with_all_versions_before_cutoff_date(
     regression_test_docmap_by_manuscript_url: str
 ):
     response = requests.get(
         url=regression_test_docmap_by_manuscript_url,
-        params={'manuscript_id': '86628'}
+        params={'manuscript_id': '86628'},
+        timeout=120
     )
     assert response.status_code == 404
 
@@ -45,7 +61,8 @@ def test_docmaps_should_not_be_available_for_manuscript_with_only_first_version_
 ):
     response = requests.get(
         url=regression_test_docmap_by_manuscript_url,
-        params={'manuscript_id': '86764'}
+        params={'manuscript_id': '86764'},
+        timeout=120
     )
     assert response.status_code == 404
 
@@ -57,12 +74,36 @@ def test_should_match_example_response(
 ):
     response = requests.get(
         url=regression_test_docmap_by_manuscript_url,
-        params={'manuscript_id': manuscript_id}
+        params={'manuscript_id': manuscript_id},
+        timeout=120
     )
     response.raise_for_status()
     with open(
         f'data/docmaps/regression_test/kotahi_docmap_by_manuscript_id/{manuscript_id}.json',
-        'r'
+        'r',
+        encoding='utf-8'
     ) as file:
         example_response = json.load(file)
     assert response.json() == example_response
+
+
+@pytest.mark.parametrize('evaluation_id', EVALUATION_ID_LIST)
+def test_should_evaluation_match_example_response(
+    regression_test_evaluations_url: str,
+    evaluation_id: str
+):
+    response = requests.get(
+        url=regression_test_evaluations_url,
+        params={'evaluation_id': evaluation_id},
+        timeout=120
+    )
+    response.raise_for_status()
+    with open(
+        f'data/docmaps/regression_test/kotahi_evaluation_by_evaluation_id/{evaluation_id}.txt',
+        'r',
+        encoding='utf-8'
+    ) as file:
+        expected_lines = [line.strip() for line in file.readlines()]
+
+    response_lines = [line.strip() for line in response.text.split("\n")]
+    assert response_lines == expected_lines
