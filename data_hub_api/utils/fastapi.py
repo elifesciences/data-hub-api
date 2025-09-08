81487af6568ccd012c175451a0c44dd4834eed13
Daniel Ecer <de-code@users.noreply.github.com>
2025-09-08 16:34:52 +0100

    Always log request id

--- data_hub_api/utils/fastapi.py
+++ data_hub_api/utils/fastapi.py
@@ -1,36 +1,43 @@
+from contextvars import ContextVar
 import logging
 import uuid
 
+
 LOGGER = logging.getLogger(__name__)
+
+
+request_id_context_var: ContextVar[str] = ContextVar('request_log_context')
+
+
+class RequestLogFilter(logging.Filter):
+    def filter(self, record):
+        request_id = request_id_context_var.get('app')
+        setattr(record, 'request_id', request_id)
+        return True
 
 
 def log_requests_middleware():
     async def log_requests(request, call_next):
         request_id = str(uuid.uuid4())
-        user_agent = request.headers.get("user-agent", "unknown")
+        user_agent = request.headers.get('user-agent', 'unknown')
         client_addr = (
             f"{request.client.host}:{request.client.port}"
             if request.client
-            else "unknown"
+            else 'unknown'
         )
-        protocol = request.scope.get("http_version", "unknown")
+        protocol = request.scope.get('http_version', 'unknown')
         method = request.method
         path = request.url.path
 
-        request.state.request_id = request_id
+        request_id_context_var.set(request_id)
 
         LOGGER.info(
             '%s',
-            f'{request_id} START {client_addr} - - "{method} {path} HTTP/{protocol}" "{user_agent}"'
+            f'Received request: {request_id} {client_addr} "{method} {path} HTTP/{protocol}"'
+            f' "{user_agent}"'
         )
 
         response = await call_next(request)
-
-        LOGGER.info(
-            '%s',
-            f'{request_id} END {client_addr} - - "{method} {path} HTTP/{protocol}"'
-            f' {response.status_code} "{user_agent}"'
-        )
 
         return response
     return log_requests
