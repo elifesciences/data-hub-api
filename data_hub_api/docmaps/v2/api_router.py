f4ed8b002506edfa282d09cd05869b612881785a
Daniel Ecer <de-code@users.noreply.github.com>
2025-09-09 14:57:47 +0100

    Reduce memory consumption when building index by streaming response

--- data_hub_api/docmaps/v2/api_router.py
+++ data_hub_api/docmaps/v2/api_router.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import APIRouter, HTTPException
+from fastapi.responses import StreamingResponse
 
 from data_hub_api.docmaps.v2.provider import DocmapsProvider
 
@@ -13,9 +14,12 @@ def create_docmaps_router(
 ) -> APIRouter:
     router = APIRouter()
 
-    @router.get("/v2/index")
-    def get_enhanced_preprints_docmaps_index():
-        return docmaps_provider.get_docmaps_index()
+    @router.get("/v2/index", response_class=StreamingResponse)
+    def stream_docmaps_index() -> StreamingResponse:
+        return StreamingResponse(
+            docmaps_provider.iter_docmaps_index_json_stream(),
+            media_type="application/json"
+        )
 
     @router.get("/v2/by-publisher/elife/get-by-manuscript-id")
     def get_enhanced_preprints_docmaps_by_manuscript_id_by_publisher_elife(manuscript_id: str):
