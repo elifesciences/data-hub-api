f8d579bd96fbaf507170865fb1e76e869fc7172c
Daniel Ecer <de-code@users.noreply.github.com>
2025-10-16 11:17:41 +0100

    Get list of regression test data files

--- data/docmaps/regression_test/update_regression_test_data.py
+++ data/docmaps/regression_test/update_regression_test_data.py
@@ -1,11 +1,18 @@
 import logging
+from pathlib import Path
 
 LOGGER = logging.getLogger(__name__)
 
 
 def main():
     LOGGER.info('Updating regression test data...')
-    # Placeholder for actual update logic
+    docmap_by_manuscript_id_data_path = (
+        Path('./data/docmaps/regression_test/docmap_by_manuscript_id')
+    )
+    json_files = list(docmap_by_manuscript_id_data_path.glob('*.json'))
+    LOGGER.info('Found %d JSON files:', len(json_files))
+    for json_file in json_files:
+        LOGGER.info('JSON file: %s', json_file)
     LOGGER.info('Regression test data updated.')
 
 
