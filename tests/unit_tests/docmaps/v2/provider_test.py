4531c1244789d9e35a85fbe0e69ff04796276f7f
Daniel Ecer <de-code@users.noreply.github.com>
2026-02-27 11:58:02 +0000

    Added test_should_convert_markdown_link_to_html_link

--- tests/unit_tests/docmaps/v2/provider_test.py
+++ tests/unit_tests/docmaps/v2/provider_test.py
@@ -47,6 +47,11 @@ class TestGetHtmlFormattedEvaluationContent:
         bold_italic_html = '<p><em><strong>bold/italics</strong> italics</em></p>'
         assert get_html_formatted_evaluation_content(bold_italic) == bold_italic_html
 
+    def test_should_convert_markdown_link_to_html_link(self):
+        assert get_html_formatted_evaluation_content(
+            '[link text](https://www.example.com)'
+        ) == '<p><a href="https://www.example.com">link text</a></p>'
+
 
 class TestDocmapsProvider:
     def test_should_create_index_with_non_empty_docmaps(
